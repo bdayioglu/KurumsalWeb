61d0c906182823039adcec6d71a2c740c3f39799
Burhan Dayıoğlu <bdayioglu@github.com>
2019-06-27 15:36:37 +0300

    Sayfalama işlemi ve kategoriye göre ürün getirme işlemi yapıldı.

--- KurumsalWebCoreMVC.UI/Controllers/ProductController.cs
+++ KurumsalWebCoreMVC.UI/Controllers/ProductController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KurumsalWeb.Business.Abstract;
+using KurumsalWeb.Entities;
+using KurumsalWebCoreMVC.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KurumsalWebCoreMVC.UI.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private IProductService _productService;
+        public ProductController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }
+
+        public IActionResult Index(int page=1,int category=0)
+        {
+            int pageSize = 10;
+            var products = _productService.GetByCategory(category);
+            ProductListViewModel model = new ProductListViewModel()
+            {
+                Products = products.Skip((page - 1) * pageSize).Take(pageSize).ToList() 
+            };
+            return View(model);
         }
     }
 }
