ac95822ad130de7c15d32c7a99cc299d88f5d7ea
Burhan Dayıoğlu <bdayioglu@github.com>
2019-06-27 18:18:31 +0300

    Kart eklemek için Cart ve CartLine adında tablolar eklendi.

---
+++ KurumsalWeb.Entities/CartLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KurumsalWeb.Entities
+{
+    public class CartLine
+    {
+        public Products Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
