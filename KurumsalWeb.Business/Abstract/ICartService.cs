b52ade1dc5cb069d0e324b064cca6c827f29628d
Burhan Dayıoğlu <bdayioglu@github.com>
2019-06-27 18:21:28 +0300

    Cart tablosu ile ilgili iş kurallarını yazmak amacıyla ICartService interface'i bunları doldurup implemente edilmiş hallerini yazmak için CartManager class'ı eklendi.

---
+++ KurumsalWeb.Business/Abstract/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using KurumsalWeb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KurumsalWeb.Business.Abstract
+{
+    public interface ICartService
+    {
+        void AddToCart(Cart cart,Products products);
+        void RemoveCart(Cart cart,  int productId);
+        List<CartLine> List(Cart cart);
+    }
+}
