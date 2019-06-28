2f300e3fa326e21712c05e0218c09343435cadd1
Burhan Dayıoğlu <bdayioglu@github.com>
2019-06-28 14:16:09 +0300

    Gerekli sayfalar eklenip düzenlemeler yapıldı.

---
+++ KurumsalWeb.Entities/ShippingDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace KurumsalWeb.Entities
+{
+    public class ShippingDetails
+    {
+        [Required]
+        public string Ad { get; set; }
+        [Required]
+        public string Soyad { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string Şehir { get; set; }
+        [Required]
+        public string Adres { get; set; }
+        [Required]
+        public string Yaş { get; set; }
+    }
+}
